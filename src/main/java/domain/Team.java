f7b5e91b800c708a738acc1047fd9f44beac1df2
Vitalii Smokov <vitalik23@windowslive.com>
2018-05-17 14:33:23 +0300

    Created class Team

---
+++ src/main/java/domain/Team.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Team {
+
+    private String coach;
+    private String city;
+    private int year;
+
+    public Team(String coach, String city, int year) {
+        this.coach = coach;
+        this.city = city;
+        this.year = year;
+    }
+}
