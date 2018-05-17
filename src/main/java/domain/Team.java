a34683280f0cd011c9185e250f85a1579a66a25a
Vitalii Smokov <vitalik23@windowslive.com>
2018-05-17 14:42:28 +0300

    Added JSON annotations to the class Team

--- src/main/java/domain/Team.java
+++ src/main/java/domain/Team.java
@@ -1,12 +1,21 @@
 package domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Team {
 
     private String coach;
     private String city;
     private int year;
 
-    public Team(String coach, String city, int year) {
+    @JsonCreator
+    public Team(
+            @JsonProperty("coach") String coach,
+            @JsonProperty("city") String city,
+            @JsonProperty("year") int year) {
         this.coach = coach;
         this.city = city;
         this.year = year;
